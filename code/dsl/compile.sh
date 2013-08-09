cfa968debc15f202e60d9b25fd9bd68e1b67c2c6
Tomo Ferega <tomo@element.hr>
2013-08-09 14:17:48 +0200

    Generated DSL classes are now in model-generated project.

--- code/dsl/compile.sh
+++ code/dsl/compile.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
 
 dir="$( dirname "$(readlink -f "$0")" )"
-out="$dir/out"
+out="$dir/../scala/model-generated/src/main"
 
-rm -rf "$out"
-
-java \
-  -jar "$dir/dsl-clc.jar" 0.7.12 \
+java -jar "$dir/dsl-clc.jar" 0.7.12 \
   --project-ini-path="~/.config/beepo/project.ini" \
   --dsl-path="$dir/model" \
   --namespace=hr.element.beepo \
   --output-path="$out" \
   --language=scala \
-	"$@"
+  "$@"
 
